6a102e4458829a9a314e884eae8768f820d675ca
kiwenlau <kiwenlau@126.com>
2015-07-21 16:28:10 +0900

    analyze pushpull benchmark 5a registry 2.0.1

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ mkdir result
 i=1
 while [ $i -le 6 ]
 do
-    rm -rf code/logs code/output0 code/output1 code/output2 >/dev/null
+    rm -rf code/logs code/output0 code/output1 code/output2 code/output3 >/dev/null
     cp -r logs/logs$i code
 	mv code/logs$i code/logs
 	cd code
     ./run.sh
 	cd ..
 	mkdir -p result/result$i
-    cp -r code/logs code/output0 code/output1 code/output2 result/result$i
+    cp -r code/logs code/output0 code/output1 code/output2 code/output3 result/result$i
     ((i++))
 done 
